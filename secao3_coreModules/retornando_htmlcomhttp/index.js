77a8eed65785bb55570558cab5bd5746d1443690
josimarsousa <josimarsousa@gmx.com>
2025-01-25 16:47:07 -0300

    teste de atualizacao

--- secao3_coreModules/retornando_htmlcomhttp/index.js
+++ secao3_coreModules/retornando_htmlcomhttp/index.js
@@ -5,7 +5,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html' )
-    res.end('<h1> Olá, este é meu primeiro server com HTML</h1>')
+    res.end('<h1> Olá, este é meu primeiro server com HTML</h1><p>testando a atualizacao do projeto</p>')
 })
 
 server.listen(port, () => {
