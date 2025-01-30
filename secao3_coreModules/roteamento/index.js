f457ba66c0eb5cf54cf61eda3258b5b33bf1d0d5
josimarsousa <josimarsousa@gmx.com>
2025-01-30 11:26:32 -0300

    roteamento terminado

--- secao3_coreModules/roteamento/index.js
+++ secao3_coreModules/roteamento/index.js
@@ -17,7 +17,11 @@ const server = http.createServer((req, res) => {
                 return res.end()
             })
         }else{
-
+            fs.readFile('404.html', function(err, data){
+                res.writeHead(404, {'content-type': 'text/html'})
+                res.write(data)
+                return res.end()
+            })
         }
     }
 
