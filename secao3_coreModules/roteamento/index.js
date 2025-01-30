ea93768750755ed826e964f3e22db4ec1c1ff375
josimarsousa <josimarsousa@gmx.com>
2025-01-30 11:08:04 -0300

    roteamento quase pronto

--- secao3_coreModules/roteamento/index.js
+++ secao3_coreModules/roteamento/index.js
@@ -1,29 +1,24 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
 const port = 3000
 
 const server = http.createServer((req, res) => {
 
-    const urlInfo = require('url').parse(req.url, true)
-    const name = urlInfo.query.name
+    const q = url.parse(req.url, true)
+    const filename = q.pathname.substring(1)
 
-    if(!name){
-        fs.readFile('escrevendo_arquivos.html', function(err, data){
-                res.writeHead(200, {'Content-Type': 'text/html'})
+    if(filename.includes('html')){
+        if(fs.existsSync(filename)){
+            fs.readFile(filename, function(err, data){
+                res.writeHead(200, {'content-type': 'text/html'})
                 res.write(data)
                 return res.end()
             })
-    }else{
+        }else{
 
-            const nameNewLine = name + ', \r\n'
-
-            fs.appendFile("arquivo.txt", nameNewLine, function (err, data){
-                res.writeHead(302, {
-                    Location: '/',
-                })
-                return res.end()
-            })
+        }
     }
 
     
