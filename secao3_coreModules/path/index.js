c5ff566975e73d4ce175d2263b58dee293c6c307
josimarsousa <josimarsousa@gmx.com>
2025-01-31 09:13:43 -0300

    path iniciado

---
+++ secao3_coreModules/path/index.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+//exemplo
+const customPath = "/relatorios/josimar/relatorio1.pdf"
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
