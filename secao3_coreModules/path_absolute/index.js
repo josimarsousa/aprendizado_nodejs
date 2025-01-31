c6319ff9ab9af8f1b47b469a0dd9ab14e69ac607
josimarsousa <josimarsousa@gmx.com>
2025-01-31 09:31:03 -0300

    ajustes finais

--- secao3_coreModules/path_absolute/index.js
+++ secao3_coreModules/path_absolute/index.js
@@ -8,6 +8,7 @@ console.log(path.resolve('teste.txt'))
 const midFolder = 'relatorios'
 const fileName = 'josimar.txt'
 
+//form finalpath e unindo tudo
 const finalPath = path.join('/', 'arquivos', midFolder, fileName)
 
 console.log(finalPath)
