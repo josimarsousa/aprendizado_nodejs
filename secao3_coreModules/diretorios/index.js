3bc10e6199e1f3004357d2b6363955997c611f27
josimarsousa <josimarsousa@gmx.com>
2025-02-01 12:24:41 -0300

    trabalhando com diretorios

---
+++ secao3_coreModules/diretorios/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+if(!fs.existsSync('./minhapasta')){
+    console.log('Não existe!')
+    fs.mkdirSync('minhapasta')
+    console.log('pasta criada agora')
+}else if(fs.existsSync('./minhapasta')){
+    console.log('Existe!')
+}
+
