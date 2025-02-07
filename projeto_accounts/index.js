e055336813c0cff0c322fc3056d0162a3a27c08e
josimarsousa <josimarsousa@gmx.com>
2025-02-07 10:13:12 -0300

    criação de diretório pronto

--- projeto_accounts/index.js
+++ projeto_accounts/index.js
@@ -1,6 +1,7 @@
 // modulos externos
 import chalk from 'chalk'
 import inquirer from 'inquirer'
+import fs from 'fs'
 
 
 Operation()
@@ -33,5 +34,30 @@ function Operation(){
 function createAccount(){
     console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco!"))
     console.log(chalk.green('Defina as opções da sua conta'))
+
+    buildAccount()
 }
 
+function buildAccount(){
+    inquirer.prompt(
+        [
+          {
+            name: 'accountName',
+            message: 'Digite um nome para sua conta:'
+          }
+             
+        ]
+    ).then((anwser) => {
+
+        const accountName = anwser['accountName']
+
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+            console.log('Diretorio criado!')
+        }
+
+    })
+    .catch((err) => console.log(err))
+}
