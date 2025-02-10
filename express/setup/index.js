d35d3bfdb4ea1acfd1d47cafc1cb30b76f1e2dce
josimarsousa <josimarsousa@gmx.com>
2025-02-09 22:02:44 -0300

    servidor rodando e code pronto

--- express/setup/index.js
+++ express/setup/index.js
@@ -2,3 +2,10 @@ const express = require('express')
 const app = express() //variavel ambiente
 const port = 3001
 
+app.get('/', (req, res) => {
+    res.send('Olá mundo')
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
