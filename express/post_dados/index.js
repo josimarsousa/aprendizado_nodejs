1f6131e621c005493c9aa7049a0a08746d0b5122
josimarsousa <josimarsousa@gmx.com>
2025-02-13 21:33:58 -0300

    post_dados criado

---
+++ express/post_dados/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const path = require('path')
+const basePath = path.join(__dirname, 'templates')
+
+app.get('/users/:id', (req, res) => {
+
+    const id = req.params.id
+
+    //leitura da tabela users, resgatar um usuário 
+    console.log(`Estamos buscando pelo usario ${id}`)
+    res.sendFile(`${basePath}/users.html`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
