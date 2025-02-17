dd2629b7f3463863320e15eee7ba0d0bfa963309
josimarsousa <josimarsousa@gmx.com>
2025-02-16 22:20:26 -0300

    post_dados pronto

--- express/post_dados/index.js
+++ express/post_dados/index.js
@@ -3,7 +3,33 @@ const app = express()
 const port = 3001
 
 const path = require('path')
+
+//ler o body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+
 const basePath = path.join(__dirname, 'templates')
+
+app.get('/users/add', (req, res)  => {
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.post('/users/save', (req, res) => { 
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuário é: ${name} e ele tem ${age} anos`)
+
+    res.sendFile(`${basePath}/userform.html`)
+})
 
 app.get('/users/:id', (req, res) => {
 
@@ -22,10 +48,3 @@ app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 })
 
-app.get('/users/add', (req, res)  => {
-    res.sendFile(`${basePath}/userform.html`)
-})
-
-app.post('/users/save', (req, res) => {
-
-})
