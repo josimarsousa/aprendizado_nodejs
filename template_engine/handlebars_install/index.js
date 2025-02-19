c5722bae8c0a2279d180a8471cba6c8366234884
josimarsousa <josimarsousa@gmx.com>
2025-02-19 10:14:39 -0300

    instalacao nodemon express_handlebars pronto

--- template_engine/handlebars_install/index.js
+++ template_engine/handlebars_install/index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
+const express = require('express') 
+const expressHandlebars = require("express-handlebars") 
 
 const app = express()
 
-app.engine('handlebars', exphbs())
+app.engine('handlebars', expressHandlebars())
+app.set('view engine', 'handlebars')
 
+app.get('/', (req, res) => {
+    res.render('home', {layout: false})
+})
+
+app.listen(3000, () => {
+    console.log('App rodando na porta 3000 ')
+})
