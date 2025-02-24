3896b44c7d3451d042faec8c09d91bd70f235e21
josimarsousa <josimarsousa@gmx.com>
2025-02-23 21:21:23 -0300

    else explicacao

---
+++ template_engine/5_else/index.js
@@ -0,0 +1,34 @@
+const express = require('express') 
+const exphbs= require("express-handlebars") 
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+app.get('/', (req, res) => {
+
+    const user = {
+        name: "josimar",
+        surname: "jose",
+        age: "30"
+    }
+
+    const palavra = "Pagina de testes"
+    //verificacao de autenticacao
+    // se condicao for falsa
+    //if const auth = false nao aparece link para navegacao
+    const auth = true
+
+    const approved = false
+
+    res.render('home', {user: user, palavra, auth, approved})
+})
+
+app.listen(3000, () => {
+    console.log('App funcionando!!!!')
+})
