c0c8289eeab4eb76c8241cb91295e57b70642663
josimarsousa <josimarsousa@gmx.com>
2025-06-01 06:33:55 -0300

    app criado

---
+++ node_com_mysql/instalcao_mysql/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
