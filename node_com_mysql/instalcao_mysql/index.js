1d7363f03392a7c8d71d16d930c54b9dbdfe5259
josimarsousa <josimarsousa@gmx.com>
2025-06-01 07:04:26 -0300

    update dependecies and connection ok

--- node_com_mysql/instalcao_mysql/index.js
+++ node_com_mysql/instalcao_mysql/index.js
@@ -1,14 +1,31 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
-const mysql = require('mysql')
+const { engine } = require('express-handlebars')
+const mysql = require('mysql2')
 
 const app = express()
 
-app.engine('handlebars', exphbs())
+app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Fridoca84*',
+    database: 'nodejs'
+})
+
+conn.connect(function(err) {
+    if (err){
+        console.log(err)
+    }
+    
+    console.log("Conectou ao Mysql!")
+    
+    app.listen(3000)
 })
