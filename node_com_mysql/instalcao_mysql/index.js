cb957b89b07de0230f0dd726eb272820bf0475bb
josimarsousa <josimarsousa@gmx.com>
2025-06-02 05:45:48 -0300

    resgatando dados e mostrando na view

--- node_com_mysql/instalcao_mysql/index.js
+++ node_com_mysql/instalcao_mysql/index.js
@@ -34,6 +34,21 @@ app.post('/books/insertbook', (req, res) => {
     } )
 })
 
+app.get('/books', (req, res) => {
+    const sql = 'SELECT * FROM books'
+
+    conn.query(sql, function(err, data){
+        if(err) {
+            console.log(err)
+            return
+        }
+        const books = data
+        console.log(books)
+
+        res.render('books', { books })
+    })
+})
+
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
