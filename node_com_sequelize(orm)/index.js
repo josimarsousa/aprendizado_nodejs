8ab3f232138d555fecef56b8dbb0fdb99ff92077
josimarsousa <josimarsousa@gmx.com>
2025-06-10 21:30:43 -0300

    rota delete pronta

--- node_com_sequelize(orm)/index.js
+++ node_com_sequelize(orm)/index.js
@@ -56,6 +56,15 @@ app.get('/users/:id', async (req, res) => {
     res.render('userview', { user })
 })
 
+app.post('/users/delete:id', async (req, res) => {
+
+    const id = req.params.id
+
+    const users = await User.destroy({where: {id: id}})
+
+    res.redirect('/')
+})
+
 conn
     .sync().then(() => {
     app.listen(3000, () => {
