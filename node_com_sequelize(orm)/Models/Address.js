1879b528f424ffe811f24cf0f4aabf31557cf064
josimarsousa <josimarsousa@gmx.com>
2025-06-17 09:36:48 -0300

    criando model relacionamentos Adress

---
+++ node_com_sequelize(orm)/Models/Address.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../connection/db/conn')
+
+const User = require('./User')
+
+const Address = db.define('Address', {
+
+
+
+})
+
+module.exports = Address
