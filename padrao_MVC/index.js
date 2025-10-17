7155b3055e6c8e1a34c6c9cc08e67dc21031f726
josimarsousa <josimarsousa@gmx.com>
2025-10-17 11:04:09 -0300

    link css ajustado e funcionando

--- padrao_MVC/index.js
+++ padrao_MVC/index.js
@@ -18,14 +18,8 @@ app.use(
     })
 )
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.js'))
-})
-
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/tasks', tasksRoutes)
 
 conn
